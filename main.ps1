7f4d92218d08f0ed80f33e864bcc66f122e59554
Anirudh <109010287+6lockk@users.noreply.github.com>
2023-04-11 17:44:08 +0530

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -1,6 +1,6 @@
 # Prompt user to select a browser to download
 $validInput = $false
-while ($validInput -eq $false) {
+while (-not $validInput) {
     Write-Host "Please select a browser to download:"
     Write-Host "1. Google Chrome"
     Write-Host "2. Mozilla Firefox"
@@ -12,14 +12,16 @@ while ($validInput -eq $false) {
 
     $input = Read-Host -Prompt "Enter a number (1-7)"
 
-    if ($input -in 1..7) {
+    if ([int]::TryParse($input, [ref]$null) -and $input -in 1..7) {
         $validInput = $true
+    } else {
+        Write-Host "Invalid input. Please enter a number between 1 and 7."
     }
 }
 
 # Download selected browser
 switch ($input) {
-    1 { Invoke-WebRequest "https://dl.google.com/chrome/install/standalone/enterprise/GoogleChromeEnterpriseBundle64.msi" -OutFile "$env:USERPROFILE\Downloads\GoogleChromeEnterpriseBundle64.msi" }
+    1 { Invoke-WebRequest "https://dl.google.com/chrome/install/standalone/GoogleChromeStandaloneEnterprise64.msi" -OutFile "$env:USERPROFILE\Downloads\GoogleChromeEnterpriseBundle64.msi" }
     2 { Invoke-WebRequest "https://download.mozilla.org/?product=firefox-latest-ssl&os=win64&lang=en-US" -OutFile "$env:USERPROFILE\Downloads\FirefoxSetup.exe" }
     3 { Invoke-WebRequest "https://github.com/Hibbiki/chromium-win64/releases/latest/download/chromium-win64.zip" -OutFile "$env:USERPROFILE\Downloads\chromium-win64.zip" }
     4 { Invoke-WebRequest "https://laptop-updates.brave.com/latest/winx64" -OutFile "$env:USERPROFILE\Downloads\BraveSetup.exe" }
